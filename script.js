471c633778d8321d239036003b2b24537689946f
dhanraj-saur <dhananjayverma301@gmail.com>
2023-02-11 17:02:37 +0530

    done

--- script.js
+++ script.js
@@ -7,21 +7,59 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
+ 
+    arr.map((ele)=>ele.marks >50 ?  console.log(ele) : "")
+  
   //Write your code here , just console.log
 }
 
 function PrintStudentsbyForEach() {
+  arr.forEach((ele)=>ele.marks >50 ?  console.log(ele) : "")
   //Write your code here , just console.log
 }
 
 function addData() {
+ let data={
+  id:4,
+  name:"susan",
+  age:"20",
+  marks:45
+ }
+ arr.push(data)
+  console.log(arr)
   //Write your code here, just console.log
 }
 
 function removeFailedStudent() {
+  arr.forEach((ele)=>ele.marks >50 ?  console.log(ele) : "")
   //Write your code here, just console.log
 }
 
 function concatenateArray() {
+  var student=[
+     {
+      id: 5,
+      name: "Dhananjay",
+      age: "22",
+      marks: 88
+     },
+    {
+      id: 6,
+      name: "Vikarm",
+      age: "21",
+      marks: 90
+    },
+    {
+      id: 7,
+      name: "Pranit",
+      age: "22",
+      marks: 92
+
+    }
+  ]
+  student.map((data)=>{
+    return arr.push(data)
+  })
+  console.log(arr)
   //Write your code here, just console.log
 }
